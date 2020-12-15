c4210bb10d25c05dc997705952b6176dcd4fc78c
Steppzilla <45403455+Steppzilla@users.noreply.github.com>
2020-12-15 16:18:42 +0100

    loesungsweg

--- src/app/components/aufgabe1/aufgabe1.component.ts
+++ src/app/components/aufgabe1/aufgabe1.component.ts
@@ -6,34 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aufgabe1.component.scss']
 })
 export class Aufgabe1Component implements OnInit {
-  show=false;
-  schritt0=false;
-  schritt1=false;
-  schritt2=false;
-  schritt3=false;
-  schritt4=false;
+  show="hide";;
+  schritt0="hide";
+  schritt1="hide";
+  schritt2="hide";
+  schritt3="hide";
+  schritt4="hide";
 
   loesunganzeigen(){
-    this.show=true;
+    this.show="show";;
   }
   schritt(zahl){
     if(zahl==0){
-      this.schritt0=true;
+      this.schritt0="show";
 
     }else if(zahl==1){
-      this.schritt1=true;
+      this.schritt1="show";
       
     }
     else if(zahl==2){
-      this.schritt2=true;
+      this.schritt2="show";
       
     }
     else if(zahl==3){
-      this.schritt3=true;
+      this.schritt3="show";
       
     }
     else if(zahl==4){
-      this.schritt4=true;
+      this.schritt4="show";
       
     }
   }
